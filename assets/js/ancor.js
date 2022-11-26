4b1a06a37f6a5afc3f3804dc43a4a0188c6f345d
Thiago Silva <euthiagosrodrigues@gmail.com>
2022-11-26 01:44:21 -0300

    update

---
+++ assets/js/ancor.js
@@ -0,0 +1,9 @@
+$('.navbar-list a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset
+	}, 1000);
+});
