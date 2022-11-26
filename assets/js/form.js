64f8b3c23c001dcc01942fb8bda1c982e7290ec5
Thiago Silva <euthiagosrodrigues@gmail.com>
2022-11-26 00:18:00 -0300

    update

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,9 @@
 const button = document.querySelector("#submit-btn");
+const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+const assunto = document.querySelector("#assunto");
+const mensagem = document.querySelector("#mensagem");
 
-button.addEventListener("click", () => {
+button.addEventListener("submit", () => {
     alert("Obrigado pelo Contato, retornarei em breve por e-mail!")
 })
